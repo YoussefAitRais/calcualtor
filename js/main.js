d2ccf9fe3902ba4c09155f8e1b5598064d9ef511
Youssef Ait Rais <copybuisness007@gmail.com>
2025-01-23 16:10:08 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let input  = document.getElementById ('input');
+let numbers = document.getElementsByClassName ('numbers');
+let operators = document.getElementsByClassName ('operators');
+let result = document.getElementById ('result');
+
+document.querySelectorAll('numbers div') 
+
+element.addEventListener ('click',()=> {
+
+});
+
+
+
+element.addEventListener("click", sum);
+
+function sum(a,b) {
+  return a + b;
+
+
+}    
+
+
+
